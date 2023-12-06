2de969213cf3e55ea3c605758f686cc5724ed661
Vinayak khavare <vinayakkhavare62@gmail.com>
2023-12-06 12:00:17 +0530

    rewrote the code

--- dsl_c19.cpp
+++ dsl_c19.cpp
@@ -11,239 +11,322 @@
 #include<string>
 using namespace std;
 
-class PinnacleClub{
-    public:
-    int prn;
-    string name;
-    PinnacleClub* next;
-
-    // Constructor
-    PinnacleClub(){
-        int n;
-        string s;
-        cout<<"Enter prn and name of student : ";
-        cin>>n>>s;
-        this->prn = n;
-        this->name = s;
-        this->next = NULL;
-    }
-
-    // Destructor
-    ~PinnacleClub(){
-        int val = this->prn;
-        string var = this->name;
-        if(this->next!=NULL){
-            delete next;
-            this->next = NULL;
-        }
-        cout<<"\nStudent with PRN no. "<<val<<" and name "<<var<<" has left the club."<<endl;
-    }
+struct node{
+	int prn;
+	string name;
+	node* next;
 };
 
-void add_member(PinnacleClub* &head){
-    PinnacleClub* temp = new PinnacleClub();
-    if(head==NULL){
-        head = temp;
-    }
-    else{
-        head->next = temp;
-    }
-}
-
-void delete_member(PinnacleClub* &head, int p){
-    PinnacleClub* curr = head;
-    PinnacleClub* prev = NULL;
-    while(curr->prn!=p){
-        prev = curr;
-        curr = curr->next;
-    }
-    prev->next = curr->next;
-    curr->next = NULL;
-//    cout<<"\nStudent with PRN no. "<<curr->prn<<" and name "<<curr->name<<" has left the club."<<endl;
-    delete curr;
-}
-
-void delete_president(PinnacleClub* &head){
-    PinnacleClub* temp = head;
-    head = temp->next;
-    temp->next = NULL;
-    delete temp;
-//    cout<<"The President has left the club."<<endl;
-}
-
-void delete_secretory(PinnacleClub* &head){
-    PinnacleClub* curr = head;
-    PinnacleClub* prev = NULL;
-    while(curr->next!=NULL){
-        prev = curr;
-        curr = curr->next;
-    }
-    prev->next = NULL;
-    delete curr;
-//    cout<<"The secretory has left the club."<<endl;
-}
-
-void replace_president(PinnacleClub* &head, int prn_of_member, string name_of_member){
-    PinnacleClub* temp = head;
-    int temp_prn = head->prn;
-    string temp_name = head->name;
-    while(temp->prn!=prn_of_member){
-        temp = temp->next;
-    }
-    head->prn = prn_of_member;
-    head->name = name_of_member;
-    temp->prn = temp_prn;
-    temp->name = temp_name;
-//    cout<<"The President has changed."<<endl;
-}
-
-void replace_secretory(PinnacleClub* &head, int prn_of_member, string name_of_member){
-    PinnacleClub* temp = head;
-    PinnacleClub* member = head;
-    while(member->prn!=prn_of_member){
-        member = member->next;
-    }
-    while(temp->next!=NULL){
-        temp = temp->next;
-    }
-    int temp_prn = temp->prn;
-    string temp_name = temp->name;
-    temp->prn = member->prn;
-    temp->name = member->name;
-    member->prn = temp_prn;
-    member->name = temp_name;
-//    cout<<"The Secretory has changed."<<endl;
-}
-
-int total_members(PinnacleClub* &head){
-    PinnacleClub* temp = head;
-    int cnt = 0;
-    do{
-        cnt++;
-        temp = temp->next;
-    }
-    while(temp->next!=NULL);
-
-    return cnt;
-}
-
-void print(PinnacleClub* &head){
-    if(head==NULL){
-        cout<<"The club is empty."<<endl;
-    }
-    PinnacleClub* temp = head;
-    int cnt = 0;
-    while(temp->next!=NULL){
-        if(cnt==0){
-        cout<<"President : "<<temp->name<<endl;
-        cout<<"Secretory : "<<temp->name<<endl;
-        cnt++;
-        }
-        cout<<"Member "<<cnt<<" : "<<temp->name<<endl;
-    }
-    cout<<"Secretory : "<<temp->name<<endl;
-}
-
-void concatenate(PinnacleClub* &h1, PinnacleClub* &h2){
-    PinnacleClub* temp = h1;
-    while(temp->next!=NULL){
-        temp = temp->next;
-    }
-    temp->next = h2;
-    h2 = h1;
-}
+class div{
+	public:
+		node* head;
+		node* tail;
+		div(){
+			head = NULL;
+			tail = NULL;
+		}
+		
+		void add(int p, string nm){
+			node* temp = new node;
+			char ch;
+			if(head==NULL){
+				temp->prn=p;
+				temp->name=nm;
+				temp->next=NULL;
+				head=temp;
+				tail=temp;
+			}
+			else{
+				node* curr = head;
+				temp->prn=p;
+				temp->name=nm;
+				temp->next=NULL;
+				while(curr->next!=tail){
+					curr=curr->next;
+				}
+				curr->next = temp;
+				temp->next = tail;
+				tail->next = NULL;
+			}
+		}
+		
+		void display(){
+			if(head==NULL){
+				cout<<"No list"<<endl;
+			}
+			else{
+				node* temp = head;
+				while(temp!=NULL){
+					cout<<"PRN: "<<temp->prn<<" Name: "<<temp->name<<endl;
+					temp=temp->next;
+				}
+			}
+		}
+		
+		void del(int p){
+			char ch;
+			if(head->prn==p){
+				cout<<"Are you trying to remove President?(Y/N)";
+				cin>>ch;
+				if(ch=='Y' || ch=='y'){
+					del_pres();
+				}
+			}
+			else if(tail->prn==p){
+				cout<<"Are you trying to remove Secretory?(Y/N)";
+				cin>>ch;
+				if(ch=='Y' || ch=='y'){
+					del_secr();
+				}
+			}
+			else{
+				node* prev = NULL;
+				node* curr = head;
+				while(curr->prn!=p){
+					prev = curr;
+					curr = curr->next;
+				}
+				prev->next=curr->next;
+				curr->next=NULL;
+				cout<<"deleted prn, "<<curr->prn<<" and name, "<<curr->name<<endl;
+				delete curr;
+			}
+		}
+		
+		void length(){
+			node* temp = head;
+			int cnt=0;
+			while(temp!=NULL){
+				cnt++;
+				temp = temp->next;
+			}
+			cout<<"total strength is, "<<cnt<<endl;
+		}
+		
+		void del_pres(){
+			cout<<"The next person in the club will be the president."<<endl;
+			node* temp = head;
+			head = head->next;
+			temp->next = NULL;
+			cout<<"Deleting president..."<<endl;
+			cout<<head->name<<" is now the President."<<endl;
+			delete temp;
+		}
+		
+		void del_secr(){
+			node* temp1 = head;
+			node* temp2 = tail;
+			while(temp1->next!=tail){
+				temp1=temp1->next;
+			}
+			temp1->next=NULL;
+			tail = temp1;
+			cout<<"Deleting secretory..."<<endl;
+			cout<<temp1->name<<" is now the Secretory."<<endl;
+			delete temp2;
+		}
+		
+		int search(int p){
+			node* temp = head;
+			while(temp!=tail){
+				if(temp->prn==p){
+					return 1;
+				}
+				else{
+					temp=temp->next;
+				}
+			}
+			if(tail->prn==p){
+				return 1;
+			}
+			else{
+				return 0;
+			}
+		}
+		
+		void add_pres(int p, string nm){
+			if(head==NULL){
+				add(p, nm);
+			}
+			else{
+				node* temp = new node;
+				temp->next = head;
+				temp->prn = p;
+				temp->name = nm;
+				head = temp;
+			}
+		}
+		
+		void add_secr(int p, string nm){
+			if(head==NULL){
+				add(p, nm);
+			}
+			else{
+				node* temp = new node;
+				temp->prn = p;
+				temp->name = nm;
+				temp->next = NULL;
+				tail->next = temp;
+				tail = temp;
+			}
+		}
+		
+		void concat(div &obj){
+			if(obj.head==NULL){
+				cout<<"No members in other div."<<endl;
+			}
+			else{
+				node* temp = tail;
+				this->tail = obj.tail;
+				temp->next = obj.head;
+				obj.head = this->head;
+				this->display();
+			}
+		}
+};
 
 int main(){
-    PinnacleClub* head = NULL;
-    // PinnacleClub* Divisons[6];
-    while(true){
-        int choice;
-        cout<<"\n01. Create the club."<<endl
-            <<"02. Add members to the club."<<endl
-            <<"03. Delete member of the club."<<endl
-            <<"04. Delete president of the club(the next person will be appointed as president)."<<endl
-            <<"05. Delete secretory of the club(the previous person will be appointed as secretory)."<<endl
-            <<"06. Replace President with an existing member of the club."<<endl
-            <<"07. Replace Secretory with an existing member of the club."<<endl
-            <<"08. Display number of existing club members."<<endl
-            <<"09. Display members of the club."<<endl
-            <<"10. Concatenate members of different divisions."<<endl
-            <<"11. Exit."<<endl;
+//	node* head = NULL;
+//	node* tail = NULL;
 
-        cout<<"\nEnter your choice: ";
-        cin>>choice;
-        if(choice==1){
-            PinnacleClub a;
-            head = &a;
-        }
-        else if(choice==2){
-            int n = 0;
-            cout<<"No of students you want to add: ";
-            cin>>n;
-            for(int i=0; i<n; i++){
-                add_member(head);
-            }
-        }
-        else if(choice==3){
-            int p;
-            cout<<"enter prn of the member : ";
-            cin>>p;
-            delete_member(head, p);
-        }
-        else if(choice==4){
-            delete_president(head);
-        }
-        else if(choice==5){
-            delete_secretory(head);
-        }
-        else if(choice==6){
-            int p;
-            string s;
-            cout<<"enter the prn and name of the elected president: ";
-            cin>>p>>s;
-            replace_president(head, p, s);
-        }
-        else if(choice==7){
-            int p;
-            string s;
-            cout<<"enter the prn and name of the elected secretory: ";
-            cin>>p>>s;
-            replace_secretory(head, p, s);
-        }
-        else if(choice==8){
-            int result = total_members(head);
-            cout<<"There are currently "<<result<<" number of member in the club including President an Secretory."<<endl;
-        }
-        else if(choice==9){
-            cout<<"\n";
-            print(head);
-            continue;
-        }
-        else if(choice==10){
-            PinnacleClub *h1, *h2;
-            int n;
-            cout<<"enter the no of members in club from divition A: ";
-            cin>>n;
-            for (int i = 0; i < n; i++)
-            {
-                add_member(h1);
-            }
-            cout<<"enter the no of members in club from divition B: ";
-            cin>>n;
-            for (int i = 0; i < n; i++)
-            {
-                add_member(h2);
-            }
-            concatenate(h1, h2);
-            print(h1);
-        }
-        else if(choice==11){
-        	cout<<"Thank You!"<<endl;
-            break;
-        }
-        else{
-            cout<<"Entered wrong choice, please enter a valid option."<<endl;
-        }
-    }
-    return 0;
+	div A, B;
+	div* d;
+	int ch;
+	char c;
+	
+	x: cout<<"enter your division(A/B): ";
+	cin>>c;
+	
+	if(c=='A'){
+		d = &A;
+	}
+	else if(c=='B'){
+		d = &B;
+	}
+	else{
+		cout<<"wrong input."<<endl;
+		goto x;
+	}
+	cout<<"\nCurrent divion is, "<<c<<endl;
+	while(true){
+		cout<<"\n****---Menu---****"
+			<<"\n01. Create Club."
+			<<"\n02. Add president."
+			<<"\n03. Add secretory."
+			<<"\n04. Add members."
+			<<"\n05. Delete president."
+			<<"\n06. Delete secretory."
+			<<"\n07. Delete members."
+			<<"\n08. total member of the club."
+			<<"\n09. display members of the club."
+			<<"\n10. concatenate the clubs."
+			<<"\n11. choose your div."
+			<<"\n12. exit."<<endl;
+		cout<<"****---****---****"<<endl;
+		
+		cout<<"Enter your choice: ";
+		cin>>ch;
+		
+		if(ch==1){
+			int p;
+			string nm;
+			cout<<"Enter prn and name of the president: ";
+			cin>>p>>nm;
+			d->add(p, nm);	
+		}
+		
+		else if(ch==2){
+			int p;
+			string nm;
+			cout<<"Enter prn and name of the president: ";
+			cin>>p>>nm;
+			int result = d->search(p);
+			if(result==1){
+				cout<<"Already exists!"<<endl;
+			}
+			else{
+				d->add_pres(p, nm);
+			}
+		}
+		
+		else if(ch==3){
+			int p;
+			string nm;
+			cout<<"Enter prn and name of the secretory: ";
+			cin>>p>>nm;
+			int result = d->search(p);
+			if(result==1){
+				cout<<"Already exists!"<<endl;
+			}
+			else{
+				d->add_secr(p, nm);
+			}	
+		}
+		
+		else if(ch==4){
+			int p;
+			string nm;
+			cout<<"Enter prn and name of the member: ";
+			cin>>p>>nm;
+			int result = d->search(p);
+			if(result==1){
+				cout<<"Already exists!"<<endl;
+			}
+			else{
+				d->add(p, nm);
+			}
+		}
+		
+		else if(ch==5){
+			d->del_pres();
+		}
+		
+		else if(ch==6){
+			d->del_secr();
+		}
+		
+		else if(ch==7){
+			int p;
+			cout<<"enter the prn of the member you want to remove: ";
+			cin>>p;
+			d->del(p);
+		}
+		
+		else if(ch==8){
+			d->length();
+		}
+		
+		else if(ch==9){
+			d->display();
+		}
+		
+		else if(ch==10){
+			char ch;
+			cout<<"Want to check members of both div befor concatenation?(Y/N) ";
+			cin>>ch;
+			if(ch=='Y' || ch=='y'){
+				cout<<" \n Div A"<<endl;
+				A.display();
+				cout<<" \n Div B"<<endl;
+				B.display();
+			}
+			cout<<"Concatenated the lists."<<endl;
+			A.concat(B);
+		}
+		
+		else if(ch==11){
+			goto x;
+		}
+		
+		else if(ch==12){
+			cout<<"Thank you."<<endl;
+			break;
+		}
+		
+		else{
+			cout<<"Wrong input."<<endl;
+		}
+	}
+	
+	return 0;
 }
